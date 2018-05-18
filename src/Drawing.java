de1ffe80927396d228cfa6129d975548ee5e0ade
rdc2 <ramon_dlc@hotmail.com>
2018-05-18 19:44:39 +0200

    Now the class drawing satisfies the requirements and pass the tests.

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Drawing extends Figure {
 
-    private final ArrayList<Figure> components = new ArrayList<>();
+    private final List<Figure> components;// = new ArrayList<>();
 
     public Drawing(double x, double y, ArrayList<Figure> components)
     {
         super(x,y);
-        this.components.addAll(components);
-        Collections.unmodifiableList(components);
+        this.components = components;
+        /*ArrayList<Figure> tmp = new ArrayList<>();
+        tmp.addAll(components);
+        this.components = Collections.unmodifiableList(tmp);*/
     }
 
-    // TODO The returned list can not be modified, but should be a Collections.unmodifiableList;
     // Getters
-    public ArrayList<Figure> getComponents()
+    public List<Figure> getComponents()
     {
-        return components;
+        //return components;
+        return Collections.unmodifiableList(components);
     }
 }
--- src/DrawingTest.java
+++ src/DrawingTest.java
@@ -1,15 +1,32 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DrawingTest {
 
     @Test
-    public void getComponents() {
+    public void componentsAreAddedByConstructor() {
         ArrayList<Figure> testComponents = new ArrayList<>();
         testComponents.add(new Circle(0.0, 0.0, 1.0));
         testComponents.add(new Rectangle(0.0, 0.0, 1.0, 2.0));
 
         Drawing testDrawing = new Drawing(0.0, 0.0, testComponents);
+
+        assertEquals(2, testDrawing.getComponents().size());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void getComponentsReturnsUnmodifiableList()
+    {
+        ArrayList<Figure> testComponents = new ArrayList<>();
+        testComponents.add(new Circle(0.0, 0.0, 1.0));
+        testComponents.add(new Rectangle(0.0, 0.0, 1.0, 2.0));
+
+        Drawing testDrawing = new Drawing(0.0, 0.0, testComponents);
+
+        List<Figure> retrievedComponents = testDrawing.getComponents();
+        retrievedComponents.add(new Circle(0.0, 0.0, 3.0));
     }
 }
