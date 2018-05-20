05deebc2737ef85b164e934e9817e71a4c4927b8
rdc2 <ramon_dlc@hotmail.com>
2018-05-20 18:28:17 +0200

    Created the basic tests for AreaDoublerTest.

--- src/AreaDoublerTest.java
+++ src/AreaDoublerTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class AreaDoublerTest {
@@ -9,16 +11,43 @@ public class AreaDoublerTest {
     private AreaDoubler testAreaDoubler;
 
     @Before
-    public void initAreaDoubler()
-    {
+    public void initAreaDoubler() {
         testAreaDoubler = new AreaDoubler();
     }
 
+    // Accept in circle doubles the area.
     @Test
     public void testVisitCircle() {
-        Circle testCircle = new Circle(0.0,0.0,2.0);
+        Circle testCircle = new Circle(0.0, 0.0, 2.0);
         Circle doubledCircle = testCircle.accept(testAreaDoubler);
+        double expected = 0.0;
 
-        assertEquals(expected, testCircle.getX(), DELTA);
+        assertEquals(expected, doubledCircle.getA(), DELTA);
+    }
+
+    // Accept in rectangle doubles the area.
+    @Test
+    public void testVisitRectangle() {
+        Rectangle testRectangle = new Rectangle(0.0, 0.0, 2.0, 3.0);
+        Rectangle doubledRectangle = testRectangle.accept(testAreaDoubler);
+        double expected = 0.0;
+
+        assertEquals(expected, doubledRectangle.getA(), DELTA);
+    }
+
+    // Accept in drawing doubles the area of all of its components.
+    @Test
+    public void testVisitDrawing() {
+        ArrayList<Figure> components = new ArrayList<>();
+        Circle testCircle = new Circle(0.0, 0.0, 2.0);
+        components.add(testCircle);
+        Drawing testDrawing = new Drawing(0.0, 0.0, components);
+        testDrawing.accept(testAreaDoubler);
+
+        ArrayList<Figure> drawingDoubledComponents = (ArrayList<Figure>) testDrawing.getComponents();
+        double expected = 0.0;
+        double actual = ((Circle) drawingDoubledComponents.get(0)).getA();
+
+        assertEquals(expected, actual, DELTA);
     }
 }
